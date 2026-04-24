6c8f67b0ba70bc4beb978a81eb1d48f3b86582bb
swin <brettjames199@gmail.com>
2026-04-24 13:19:26 +1000

    added garbage collection

--- modules/hosts/styx/configuration.nix
+++ modules/hosts/styx/configuration.nix
@@ -10,6 +10,13 @@
     ];
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    nix.settings.auto-optimise-store = true;
     
     # Bootloader.
     boot.loader.systemd-boot.enable = true;
