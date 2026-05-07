882b72a6a86458b799cf11e26c0798a65e596150
Brett James <brettjames199@gmail.com>
2026-05-07 12:38:41 +1000

    hardware conf

--- modules/hosts/void/hardware-configuration.nix
+++ modules/hosts/void/hardware-configuration.nix
@@ -7,30 +7,31 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  # TODO: Update with hardware-specific configuration when base OS is setup
-
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/CHANGE-ME";
+    { device = "/dev/mapper/luks-4f9f3478-a753-41c5-b585-66b8e75384aa";
       fsType = "ext4";
     };
 
+  boot.initrd.luks.devices."luks-4f9f3478-a753-41c5-b585-66b8e75384aa".device = "/dev/disk/by-uuid/4f9f3478-a753-41c5-b585-66b8e75384aa";
+
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/CHANGE-ME";
+    { device = "/dev/disk/by-uuid/42B3-9546";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/CHANGE-ME"; }
+    [ { device = "/dev/mapper/luks-33076ee5-1fd4-49b1-9c28-8ee4e3a26b74"; }
     ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-}; 
+};
 
 }
+
