ea1238a8f5c465d7fd4f7c81423592470a52f52a
swin <brettjames199@gmail.com>
2026-05-12 22:18:28 +1000

    upg

--- modules/features/fileManager.nix
+++ modules/features/fileManager.nix
@@ -1,5 +1,22 @@
 { self, inputs, ... }: {
-  flake.nixosModules.fileManager = { config, pkgs, lib, ... }: {
+  flake.nixosModules.fileManager = { config, pkgs, lib, ... }:
+  let
+    oxocarbon-gtk-theme = pkgs.stdenv.mkDerivation {
+      pname = "oxocarbon-gtk-theme";
+      version = "unstable-2023";
+      src = pkgs.fetchzip {
+        url = "https://git.sr.ht/~ved/oxocarbon-gtk/archive/0cf0eb35a927bffcb797db8a074ce240823d92de.tar.gz";
+        hash = "sha256-URuoDJVRQ05S+u7mkz1EN5HWquhTC4OqY8MqAbl0crk=";
+      };
+      nativeBuildInputs = [ pkgs.dart-sass ];
+      buildPhase = "sass scss:.";
+      installPhase = ''
+        install -d $out/share/themes/oxocarbon
+        install -m 0644 index.theme $out/share/themes/oxocarbon/
+        cp -r assets gtk-3.0 $out/share/themes/oxocarbon/
+      '';
+    };
+  in {
     services.gvfs.enable = true;
 
     # programs.nautilus-open-any-terminal = {
@@ -12,6 +29,8 @@
       settings = {
         "org/gnome/desktop/interface" = {
           icon-theme = "Papirus-Dark";
+          gtk-theme = "oxocarbon";
+          color-scheme = "prefer-dark";
         };
         # "org/gnome/nautilus/preferences" = {
         #   show-hidden-files = true;
@@ -33,6 +52,7 @@
       nemo-with-extensions
       xdg-utils
       papirus-icon-theme
+      oxocarbon-gtk-theme
     ];
   };
 }
